54bdcc1f60bd59380e7eef7b84896863aca14f42
adnan ahmed <adnanahmed504423@gmail.com>
2025-07-26 13:57:33 +0530

    logics for number based

--- numbers/factorialofnum.dart
+++ numbers/factorialofnum.dart
@@ -6,7 +6,8 @@ print(result);
 }
 int factorialnumber(int n){
   int factorial=1;
-  for(int i=1;i<=n;i++){
+  for(int i=1;i<=n;i++){//1*1=1 i+1 i=2 factorial=1 i*2=2 factorial 2 i=3 factorial 2*3=6 i=4 factorial =6 i*factorial 6*4=24 factorial 24*5 =120 and condition pass
+  
     factorial*=i;
 
   }
---
+++ numbers/reversenumber.dart
@@ -0,0 +1,14 @@
+void main(){
+  int n=123;
+  int result=reverserd(n);
+  print("reversed number is $result");
+}
+int reverserd(int n){
+  int rev=0;
+  while(n>0){
+    int lastdigit=n%10;
+    rev=rev*10+lastdigit;
+    n=n~/10;
+  }
+  return rev;
+}
