6822c44e2dbffade6b8eb006cbfe2ceeefe28d18
adnan ahmed <adnanahmed504423@gmail.com>
2025-07-30 00:00:08 +0530

    minor changes

--- numbers/sumn.dart
+++ numbers/sumn.dart
@@ -8,8 +8,8 @@ void main(){
 int sumnnumber(int n){
     int sum=0;
     for(int i=1;i<=n;i++){
-        sum=sum+i;//0+1=1 1+2=3 3+3=6 6+4=10 10+5=15
+        sum=sum+i;
 
     }
-    return sum;
+    return ;
 }
