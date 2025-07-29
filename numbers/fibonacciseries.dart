45562948c411b8fbcf49b9b270c1db35fe70b76e
adnan ahmed <adnanahmed504423@gmail.com>
2025-07-29 23:58:47 +0530

    fiboacci number

---
+++ numbers/fibonacciseries.dart
@@ -0,0 +1,13 @@
+//Print Fibonacci series up to N terms
+
+void main(){
+  int n=10;
+  for(int i=0;i<n;i++){
+print(Fibonacci(i));
+  }
+}
+int Fibonacci(int n){
+  if(n==0) return 0;
+  if(n==1) return 1;
+  return Fibonacci(n-1)+Fibonacci(n-2);
+}
